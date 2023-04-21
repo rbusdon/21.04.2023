75a4a17ffecc0802fc07aeb4e7dde94e540b46f2
Rocco Busdon <126678059+rbusdon@users.noreply.github.com>
2023-04-21 10:20:37 +0200

    Add Script2

---
+++ Esercizio1_Script2.sql
@@ -0,0 +1,17 @@
+INSERT INTO MUSEUM VALUES(1, 'Santa Maria Gloriosa dei Frari', 'Venezia');
+INSERT INTO MUSEUM VALUES(2, 'Louvre', 'Parigi');
+INSERT INTO MUSEUM VALUES(3, 'Galleria Borghese', 'Roma');
+INSERT INTO MUSEUM VALUES(4, 'Art Institute of Chicago', 'Chicago');
+
+INSERT INTO ARTIST VALUES(1, 'Tiziano Vecellio', 'Italia');
+INSERT INTO ARTIST VALUES(2, 'Caravaggio', 'Italia');
+INSERT INTO ARTIST VALUES(3, 'Picasso', 'Spagna');
+
+INSERT INTO CHARCATER VALUES(1, 'Flora');
+INSERT INTO CHARCATER VALUES(2, 'Davide');
+INSERT INTO CHARCATER VALUES(3, 'Chitarrista');
+
+INSERT INTO ARTWORK VALUES(1, 'Flora', 1, 1, 1);
+INSERT INTO ARTWORK VALUES(2, 'Concerto campestre', 2, 1, NULL);
+INSERT INTO ARTWORK VALUES(3, 'Davide con la testa di Golia', 3, 2, 2);
+INSERT INTO ARTWORK VALUES(4, 'Il vecchio chitarrista cieco', 4, 3, 3);
